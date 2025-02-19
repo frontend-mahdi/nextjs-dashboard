639139846dc7b4d47f593d25a26601ee3bc9f636
frontend-mahdi <mahdi.313.ab@gmail.com>
2025-02-19 23:13:29 +0330

    Chapter-16 Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,18 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { Metadata } from "next";
+
+// export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+//   const invoice = await fetchInvoiceById(params.id);
+//   return {
+//     title: `Edit Invoice #${invoice.id} | Dashboard`,
+//   };
+// }
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
